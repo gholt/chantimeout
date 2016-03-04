3e576f1a26b494f50d4b4f54e5eef20d199aa4fc
Greg Holt <gregory_holt@icloud.com>
2016-03-03 18:29:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Tests using Go channels and timeouts
+
+I was experiencing bad behavior with Go's time.After and I was curious what the
+deal might be. So I wrote some code that sends a ton of messages through a
+channel with no timeout code whatsoever, then with time.After, and finally with
+a _fuzzy_ time.Ticker. Here's the output I got:
+
+```
+noTimeout: 238ns/message, 2.386396812s elapsed
+    0 sys, 19 mallocs
+timeAfter: 1183ns/message, 11.831025053s elapsed, 0 timeouts
+    490408864 sys, 30001979 mallocs
+ticker:    407ns/message, 4.075570391s elapsed, 0 timeouts
+    0 sys, 17 mallocs
+```
+
+Okay, speed first, time.After takes five times as long per message whereas the
+time.Ticker takes less than twice as long when comparing them to the no timeout
+code.
+
+But honestly, it's the secondary part that's even more troubling. time.After
+causes half a gigabyte of memory allocations over thirty million mallocs!
+That's insane. I understand it's because we're creating a new *thing* for each
+message; it's just that maybe there should be a note in the documentation
+discouraging heavy use of this. "May cause destructive use of memory and thrash
+the garbage collector; only use in extreme moderation."
+
+The time.Ticker code isn't going to give you a perfect one second timeout;
+it'll be somewhere between just over half a second to just under a second. But
+it could be modified to have a better resolution if needed.
+
+Have I completely missed something obvious? A good way to use time.After? Maybe
+some other way of doing timeouts? If so, please, please let me know by popping
+a GitHub issue on this repository. Thanks!
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+const COUNT = 10000000
+
+func main() {
+	memStats := &runtime.MemStats{}
+	runtime.ReadMemStats(memStats)
+	beginMemSys := memStats.Sys
+	beginMemMallocs := memStats.Mallocs
+
+	noTimeout()
+	runtime.ReadMemStats(memStats)
+	endMemSys := memStats.Sys
+	endMemMallocs := memStats.Mallocs
+	fmt.Printf("    %d sys, %d mallocs\n", endMemSys-beginMemSys, endMemMallocs-beginMemMallocs)
+	beginMemSys = endMemSys
+	beginMemMallocs = endMemMallocs
+
+	timeAfter()
+	runtime.ReadMemStats(memStats)
+	endMemSys = memStats.Sys
+	endMemMallocs = memStats.Mallocs
+	fmt.Printf("    %d sys, %d mallocs\n", endMemSys-beginMemSys, endMemMallocs-beginMemMallocs)
+	beginMemSys = endMemSys
+	beginMemMallocs = endMemMallocs
+
+	ticker()
+	runtime.ReadMemStats(memStats)
+	endMemSys = memStats.Sys
+	endMemMallocs = memStats.Mallocs
+	fmt.Printf("    %d sys, %d mallocs\n", endMemSys-beginMemSys, endMemMallocs-beginMemMallocs)
+}
+
+func noTimeout() {
+	begin := time.Now()
+	type message struct{}
+	messageChan := make(chan *message)
+	doneChan := make(chan struct{})
+	go func() {
+		for {
+			if <-messageChan == nil {
+				doneChan <- struct{}{}
+				return
+			}
+		}
+	}()
+	for i := 0; i < COUNT; i++ {
+		messageChan <- &message{}
+	}
+	close(messageChan)
+	<-doneChan
+	elapsed := time.Since(begin)
+	fmt.Printf("noTimeout: %dns/message, %s elapsed\n", int64(elapsed)/COUNT, elapsed)
+}
+
+func timeAfter() {
+	begin := time.Now()
+	type message struct{}
+	messageChan := make(chan *message)
+	doneChan := make(chan struct{})
+	go func() {
+		for {
+			if <-messageChan == nil {
+				doneChan <- struct{}{}
+				return
+			}
+		}
+	}()
+	var timeouts int
+	for i := 0; i < COUNT; i++ {
+		select {
+		case messageChan <- &message{}:
+		case <-time.After(time.Second):
+			timeouts++
+		}
+	}
+	close(messageChan)
+	<-doneChan
+	elapsed := time.Since(begin)
+	fmt.Printf("timeAfter: %dns/message, %s elapsed, %d timeouts\n", int64(elapsed)/COUNT, elapsed, timeouts)
+}
+
+func ticker() {
+	begin := time.Now()
+	type message struct{}
+	messageChan := make(chan *message)
+	doneChan := make(chan struct{})
+	go func() {
+		for {
+			if <-messageChan == nil {
+				doneChan <- struct{}{}
+				return
+			}
+		}
+	}()
+	ticker := time.NewTicker(time.Second)
+	var timeouts int
+	for i := 0; i < COUNT; i++ {
+		for j := 0; j < 2; j++ {
+			select {
+			case messageChan <- &message{}:
+				j = 2
+			case <-ticker.C:
+				if j == 1 {
+					timeouts++
+				}
+			}
+		}
+	}
+	ticker.Stop()
+	close(messageChan)
+	<-doneChan
+	elapsed := time.Since(begin)
+	fmt.Printf("ticker:    %dns/message, %s elapsed, %d timeouts\n", int64(elapsed)/COUNT, elapsed, timeouts)
+}
